5bbd68458433ce26ab9307045514482191c292fe
Michael Owen <m.harry.owen@gmail.com>
2023-10-02 16:10:09 -0400

    tweak build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'SynapseBg',
+      fileName: (format) => `vue3-synapse-bg.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [vue()],
+});
